66c10f2466cdd430f33187803bdc696e8b3bce9b
agoloman <agoloman@mozilla.com>
2025-08-29 03:37:37 +0300

    Revert "Bug 1959094 - Display an error when an unsupported file is uploaded as a Custom wallpaper. r=home-newtab-reviewers,maxx,fluent-reviewers,bolsson" for causing newtab failures.
    This reverts commit a02a7e2779c1f046cacfd5acb34479dd6089ff7d.

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -13957,7 +13957,7 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
       showColorPicker: false,
       inputType: "radio",
       activeId: null,
-      customWallpaperErrorType: null
+      isCustomWallpaperError: false
     };
   }
   componentDidMount() {
@@ -14143,40 +14143,31 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
     // Catch cancel events
     fileInput.oncancel = async () => {
       this.setState({
-        customWallpaperErrorType: null
+        isCustomWallpaperError: false
       });
     };
 
     // Reset error state when user begins file selection
     this.setState({
-      customWallpaperErrorType: null
+      isCustomWallpaperError: false
     });
 
     // Fire when user selects a file
     fileInput.onchange = async event => {
       const [file] = event.target.files;
-      if (file) {
-        // Validate file type: Only accept files with a valid image MIME type
-        const isValidImage = file.type && file.type.startsWith("image/");
-        if (!isValidImage) {
-          console.error("Invalid file type");
-          this.setState({
-            customWallpaperErrorType: "fileType"
-          });
-          return;
-        }
 
-        // Limit image uploaded to a maximum file size if enabled
-        // Note: The max file size pref (customWallpaper.fileSize) is converted to megabytes (MB)
-        // Example: if pref value is 5, max file size is 5 MB
-        const maxSize = wallpaperUploadMaxFileSize * 1024 * 1024;
-        if (wallpaperUploadMaxFileSizeEnabled && file.size > maxSize) {
-          console.error("File size exceeds limit");
-          this.setState({
-            customWallpaperErrorType: "fileSize"
-          });
-          return;
-        }
+      // Limit image uploaded to a maximum file size if enabled
+      // Note: The max file size pref (customWallpaper.fileSize) is converted to megabytes (MB)
+      // Example: if pref value is 5, max file size is 5 MB
+      const maxSize = wallpaperUploadMaxFileSize * 1024 * 1024;
+      if (wallpaperUploadMaxFileSizeEnabled && file && file.size > maxSize) {
+        console.error("File size exceeds limit");
+        this.setState({
+          isCustomWallpaperError: true
+        });
+        return;
+      }
+      if (file) {
         this.props.dispatch(actionCreators.OnlyToMain({
           type: actionTypes.WALLPAPER_UPLOAD,
           data: file
@@ -14375,26 +14366,15 @@ class _WallpaperCategories extends (external_React_default()).PureComponent {
         htmlFor: category,
         "data-l10n-id": fluent_id
       }, fluent_id));
-    })), this.state.customWallpaperErrorType && /*#__PURE__*/external_React_default().createElement("div", {
+    })), this.state.isCustomWallpaperError && /*#__PURE__*/external_React_default().createElement("div", {
       className: "custom-wallpaper-error",
       id: "customWallpaperError"
     }, /*#__PURE__*/external_React_default().createElement("span", {
       className: "icon icon-info"
-    }), (() => {
-      switch (this.state.customWallpaperErrorType) {
-        case "fileSize":
-          return /*#__PURE__*/external_React_default().createElement("span", {
-            "data-l10n-id": "newtab-wallpaper-error-max-file-size",
-            "data-l10n-args": `{"file_size": ${wallpaperUploadMaxFileSize}}`
-          });
-        case "fileType":
-          return /*#__PURE__*/external_React_default().createElement("span", {
-            "data-l10n-id": "newtab-wallpaper-error-file-type"
-          });
-        default:
-          return null;
-      }
-    })())), /*#__PURE__*/external_React_default().createElement(external_ReactTransitionGroup_namespaceObject.CSSTransition, {
+    }), /*#__PURE__*/external_React_default().createElement("span", {
+      "data-l10n-id": "newtab-wallpaper-error-max-file-size",
+      "data-l10n-args": `{"file_size": ${wallpaperUploadMaxFileSize}}`
+    }))), /*#__PURE__*/external_React_default().createElement(external_ReactTransitionGroup_namespaceObject.CSSTransition, {
       in: !!activeCategory,
       timeout: 300,
       classNames: "wallpaper-list",
