8bdae2e8ab0cba3b4cbf23f7a591f35eb666e237
Lee Salzman <lsalzman@mozilla.com>
2025-09-02 14:15:04 +0000

    Bug 1986185. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D263287

--- gfx/layers/ipc/CanvasTranslator.cpp
+++ gfx/layers/ipc/CanvasTranslator.cpp
@@ -188,6 +188,10 @@ mozilla::ipc::IPCResult CanvasTranslator::RecvInitTranslator(
   }
 
   // Use the first buffer as our current buffer.
+  if (aBufferHandles.IsEmpty()) {
+    Deactivate();
+    return IPC_FAIL(this, "No canvas buffer shared memory supplied.");
+  }
   mDefaultBufferSize = aBufferHandles[0].Size();
   auto handleIter = aBufferHandles.begin();
   mCurrentShmem.shmem = std::move(*handleIter).Map();
@@ -443,11 +447,19 @@ void CanvasTranslator::RemoveExportSurface(gfx::ReferencePtr aRefPtr) {
 }
 
 void CanvasTranslator::RecycleBuffer() {
+  if (!mCurrentShmem.IsValid()) {
+    return;
+  }
+
   mCanvasShmems.emplace(std::move(mCurrentShmem));
   NextBuffer();
 }
 
 void CanvasTranslator::NextBuffer() {
+  if (mCanvasShmems.empty()) {
+    return;
+  }
+
   // Check and signal the writer when we finish with a buffer, because it
   // might have hit the buffer count limit and be waiting to use our old one.
   CheckAndSignalWriter();
