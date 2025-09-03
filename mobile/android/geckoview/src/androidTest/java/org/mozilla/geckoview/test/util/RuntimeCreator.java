5a3fc7fc2be5000470b56c982adad28c2c59bd47
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2025-09-03 11:56:02 +0000

    Bug 1977950 - Add an API to enable isolated process for content service. r=geckoview-reviewers,geckoview-api-reviewers,ohall,calu
    Actually, we have both build type for normal and isolated content process.
    I would like to integrate both into single GeckoView package.
    
    As first step, I add an API to enable isolated content process.
    
    Although we still keep both build types by this fix, the different is
    test runner binary only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D259509

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/util/RuntimeCreator.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/util/RuntimeCreator.java
@@ -13,6 +13,7 @@ import androidx.annotation.UiThread;
 import androidx.test.platform.app.InstrumentationRegistry;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.json.JSONObject;
+import org.mozilla.geckoview.BuildConfig;
 import org.mozilla.geckoview.ContentBlocking;
 import org.mozilla.geckoview.ExperimentDelegate;
 import org.mozilla.geckoview.GeckoPreferenceController;
@@ -199,6 +200,7 @@ public class RuntimeCreator {
             .consoleOutput(true)
             .crashHandler(TestCrashHandler.class)
             .experimentDelegate(sRuntimeExperimentDelegateProxy)
+            .isolatedProcessEnabled(BuildConfig.MOZ_ANDROID_CONTENT_SERVICE_ISOLATED_PROCESS)
             .build();
 
     sRuntime =
