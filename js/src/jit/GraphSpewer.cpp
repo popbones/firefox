334a4c72914ec5279d1962798a986ed471c4911c
Ryan Hunt <rhunt@eqrion.net>
2025-09-05 21:16:03 +0000

    Bug 1987206 - Update iongraph.json spewer. r=bvisness
    1. Add version field
     2. Rename MIR/LIR block 'number' to be 'id'
     3. Rename MIR/LIR block 'id' to be 'ptr'
     4. Add MDef 'ptr'
     5. Add LInstruction 'ptr'
     6. Add LInstruction 'mirPtr' (may be null)
     7. Start wrapping the single function from IonPerfSpewer with wrapper object and version
    
    Differential Revision: https://phabricator.services.mozilla.com/D263958

--- js/src/jit/GraphSpewer.cpp
+++ js/src/jit/GraphSpewer.cpp
@@ -18,6 +18,19 @@
 using namespace js;
 using namespace js::jit;
 
+static constexpr uint32_t IonGraphVersion = 1;
+
+// Hash pointers to make them smaller, while still (probably) unique.
+static uint32_t HashedPointer(const void* pointer) {
+  return mozilla::HashGeneric((uintptr_t)pointer);
+}
+
+void GraphSpewer::begin() {
+  beginObject();
+  property("version", IonGraphVersion);
+  beginListProperty("functions");
+}
+
 void GraphSpewer::beginFunction(JSScript* script) {
   beginObject();
   formatProperty("name", "%s:%u", script->filename(), script->lineno());
@@ -82,6 +95,7 @@ void GraphSpewer::spewMResumePoint(MResumePoint* rp) {
 void GraphSpewer::spewMDef(MDefinition* def) {
   beginObject();
 
+  property("ptr", HashedPointer(def));
   property("id", def->id());
 
   propertyName("opcode");
@@ -148,11 +162,6 @@ void GraphSpewer::spewMDef(MDefinition* def) {
   endObject();
 }
 
-static uint32_t StableBlockId(const MBasicBlock* block) {
-  uintptr_t pointer = (uintptr_t)block;
-  return mozilla::HashGeneric(pointer);
-}
-
 void GraphSpewer::spewMIR(MIRGraph* mir) {
   beginObjectProperty("mir");
   beginListProperty("blocks");
@@ -160,14 +169,8 @@ void GraphSpewer::spewMIR(MIRGraph* mir) {
   for (MBasicBlockIterator block(mir->begin()); block != mir->end(); block++) {
     beginObject();
 
-    // iongraph needs both the block 'number' (which is unstable and changes as
-    // blocks are moved around), and also a stable 'id' it can use to not lose
-    // focus as the user switches passes.
-    //
-    // Note: the terminology here is backwards from on MBasicBlock for backwards
-    // compat reasons.
-    property("id", StableBlockId(*block));
-    property("number", block->id());
+    property("ptr", HashedPointer(*block));
+    property("id", block->id());
     property("loopDepth", block->loopDepth());
 
     beginListProperty("attributes");
@@ -219,7 +222,13 @@ void GraphSpewer::spewMIR(MIRGraph* mir) {
 void GraphSpewer::spewLIns(LNode* ins) {
   beginObject();
 
+  property("ptr", HashedPointer(ins));
   property("id", ins->id());
+  if (ins->mirRaw()) {
+    property("mirPtr", HashedPointer(ins->mirRaw()));
+  } else {
+    nullProperty("mirPtr");
+  }
 
   propertyName("opcode");
   out_.printf("\"");
@@ -250,7 +259,8 @@ void GraphSpewer::spewLIR(MIRGraph* mir) {
     }
 
     beginObject();
-    property("number", i->id());
+    property("id", i->id());
+    property("ptr", HashedPointer(*i));
 
     beginListProperty("instructions");
     for (size_t p = 0; p < block->numPhis(); p++) {
@@ -314,6 +324,11 @@ void GraphSpewer::spewRanges(BacktrackingAllocator* regalloc) {
 void GraphSpewer::endPass() { endObject(); }
 
 void GraphSpewer::endFunction() {
+  endList();
+  endObject();
+}
+
+void GraphSpewer::end() {
   endList();
   endObject();
 }
