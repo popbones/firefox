0540696d415565a3bad01e905745efadeb74e0e7
Emilio Cobos Álvarez <emilio@crisal.io>
2025-09-06 21:26:43 +0000

    Bug 1964901 - Don't rely on the child having sent us the supported command list for global commands. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D263898

--- toolkit/actors/ControllersParent.sys.mjs
+++ toolkit/actors/ControllersParent.sys.mjs
@@ -29,18 +29,25 @@ export class ControllersParent extends JSWindowActorParent {
       this.supportedCommands[command] = false;
     }
 
-    let browser = this.browser;
-    if (browser) {
-      browser.ownerGlobal.updateCommands(aAction);
-    }
+    this.browser?.ownerGlobal.updateCommands(aAction);
   }
 
   isCommandEnabled(aCommand) {
-    return this.supportedCommands[aCommand] || false;
+    // Some commands are always supported or enabled for a given window.
+    // We don't want to rely on the child having sent us its command list, so
+    // check with ChromeUtils.
+    return (
+      this.supportedCommands[aCommand] ||
+      !!ChromeUtils.getGlobalWindowCommand(aCommand)
+    );
   }
 
   supportsCommand(aCommand) {
-    return aCommand in this.supportedCommands;
+    if (aCommand in this.supportedCommands) {
+      return true;
+    }
+    // See isCommandEnabled().
+    return ChromeUtils.getGlobalWindowCommand(aCommand) !== null;
   }
 
   doCommand(aCommand) {
