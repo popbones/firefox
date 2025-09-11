81c9d78050a6f01de01238337062718a3ea28ab3
Olivia Hall <ohall@mozilla.com>
2025-09-11 01:26:44 +0000

    Bug 1987152 - Disable Tests with Intermittent Failures on Android 14 r=geckoview-reviewers,tcampbell
    This patch is to @Ignore tests that appear to have frequent intermittent failures on Android 14.
    Bug for reopening them is bug 1988041.
    
    Differential Revision: https://phabricator.services.mozilla.com/D264487

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/MediaSessionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/MediaSessionTest.kt
@@ -14,6 +14,7 @@ import org.hamcrest.Matchers.notNullValue
 import org.junit.After
 import org.junit.Assume.assumeThat
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mozilla.geckoview.GeckoResult
@@ -80,6 +81,7 @@ class MediaSessionTest : BaseSessionTest() {
     fun teardown() {
     }
 
+    @Ignore("https://bugzilla.mozilla.org/show_bug.cgi?id=1988041")
     @Test
     fun domMetadataPlayback() {
         val onActivatedCalled = arrayOf(GeckoResult<Void>())
@@ -822,6 +824,7 @@ class MediaSessionTest : BaseSessionTest() {
         sessionRule.waitForResult(completedStep8)
     }
 
+    @Ignore("https://bugzilla.mozilla.org/show_bug.cgi?id=1988041")
     @Test
     fun fullscreenVideoElementMetadata() {
         // Bug 1981579
