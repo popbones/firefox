71089db6fde8d31aa83c89a3a262afeb35e47a09
alwu <alwu@mozilla.com>
2025-09-15 17:48:28 +0000

    Bug 1988291 - update nestegg to a507ba1. r=media-playback-reviewers,kinetik
    Differential Revision: https://phabricator.services.mozilla.com/D264667

--- media/libnestegg/include/nestegg/nestegg.h
+++ media/libnestegg/include/nestegg/nestegg.h
@@ -387,6 +387,14 @@ int nestegg_read_last_packet(nestegg * context,
                              unsigned int track,
                              nestegg_packet ** packet);
 
+/** Read total frame count for a track without affecting current parser state.\
+    This MUST be called before any call to nestegg_read_packet!
+    @param context    Stream context initialized by #nestegg_init.
+    @param frames_out Storage for the returned total frames count.
+    @retval 0         Success.
+    @retval -1        Error. */
+int nestegg_read_total_frames_count(nestegg * context, uint64_t * frames_out);
+
 /** Destroy a nestegg_packet and free associated memory.
     @param packet #nestegg_packet to be freed. @see nestegg_read_packet */
 void nestegg_free_packet(nestegg_packet * packet);
