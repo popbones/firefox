67ae8c042d0ff6b4d2b52a9ac9e708f7137fdee0
Nika Layzell <nika@thelayzells.com>
2025-09-17 15:06:36 +0000

    Bug 1987977 - Add extra fd validation to ChannelPosix, r=ipc-reviewers,jld
    Every attached FD has a guaranteed 4 bytes of payload, so this check
    should be redundant unless a message payload is manually constructed or
    corrupted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265038

--- ipc/chromium/src/chrome/common/ipc_channel_posix.cc
+++ ipc/chromium/src/chrome/common/ipc_channel_posix.cc
@@ -423,8 +423,9 @@ bool ChannelPosix::ProcessIncomingMessages() {
           error = "Message needs unreceived descriptors";
         }
 
-        if (m.header()->num_handles >
-            IPC::Message::MAX_DESCRIPTORS_PER_MESSAGE) {
+        size_t maxHandles = std::min<size_t>(
+            m.size(), IPC::Message::MAX_DESCRIPTORS_PER_MESSAGE);
+        if (m.header()->num_handles > maxHandles) {
           // There are too many descriptors in this message
           error = "Message requires an excessive number of descriptors";
         }
@@ -540,8 +541,9 @@ bool ChannelPosix::ProcessOutgoingMessages() {
       }
 #endif
 
-      if (msg->attached_handles_.Length() >
-          IPC::Message::MAX_DESCRIPTORS_PER_MESSAGE) {
+      size_t maxHandles = std::min<size_t>(
+          msg->size(), IPC::Message::MAX_DESCRIPTORS_PER_MESSAGE);
+      if (msg->attached_handles_.Length() > maxHandles) {
         MOZ_DIAGNOSTIC_CRASH("Too many file descriptors!");
         CHROMIUM_LOG(FATAL) << "Too many file descriptors!";
         // This should not be reached.
