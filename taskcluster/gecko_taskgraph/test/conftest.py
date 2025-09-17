0b8bace7373c0948554aaf6bae861301f6591056
Cosmin Sabou <csabou@mozilla.com>
2025-09-17 22:58:18 +0300

    Revert "Bug 1982882: disable parallel taskgraph for decision tasks r=taskgraph-reviewers,jcristau" for causing ci failures.
    This reverts commit c3922ae6253e973126571c9f9ecea183ffe42e14.
    
    Revert "Bug 1982882: vendor taskgraph 15.2.1 r=taskgraph-reviewers,mach-reviewers,jcristau"
    
    This reverts commit 11a55294c3f8d5e88368c10763842bb7fa63287b.
    
    Revert "Bug 1982882: fix gecko_taskgraph tests to work with multiprocess taskgraph r=taskgraph-reviewers,jcristau"
    
    This reverts commit 9c615b20b66f2aa13a08412bb0edb2ce0d684f32.
    
    Revert "Bug 1982882: run taskgraph-diff in serial mode r=taskgraph-reviewers,jcristau"
    
    This reverts commit 83b03720ce6f9e8d17a05ab88d6ebbaba2835ddc.

--- taskcluster/gecko_taskgraph/test/conftest.py
+++ taskcluster/gecko_taskgraph/test/conftest.py
@@ -119,15 +119,11 @@ class WithFakeKind(TaskGraphGenerator):
             yield FakeKind.create(kind_name, cfg, graph_config)
 
 
-class FakeGraphConfig(GraphConfig):
-    def register(self):
-        pass
-
-
 def fake_load_graph_config(root_dir):
-    graph_config = FakeGraphConfig(
+    graph_config = GraphConfig(
         {"trust-domain": "test-domain", "taskgraph": {}}, root_dir
     )
+    graph_config.__dict__["register"] = lambda: None
     return graph_config
 
 
