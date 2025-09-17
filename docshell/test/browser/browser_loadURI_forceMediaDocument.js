30421317e6a9a99a5a81b0ff9807f5103ab068c6
Alexandru Marc <amarc@mozila.com>
2025-09-17 11:02:50 +0300

    Revert "Bug 1987407 - Make forceMediaDocument: "image" work for SVGs. r=smaug" for causing build bustages @ nsContentDLF.cpp
    This reverts commit 9c21055edfa2a3e26739f241a883e8b9450cf3e8.

--- docshell/test/browser/browser_loadURI_forceMediaDocument.js
+++ docshell/test/browser/browser_loadURI_forceMediaDocument.js
@@ -5,34 +5,23 @@ const TEST_PATH = getRootDirectory(gTestPath).replace(
   "chrome://mochitests/content",
   "https://example.com"
 );
+const TEST_IMG_PATH = TEST_PATH + "file_image_header.sjs?image";
 
-function doLoad(url, forceMediaDocument, contentFn) {
-  return BrowserTestUtils.withNewTab({ gBrowser }, async function (browser) {
-    browser.loadURI(Services.io.newURI(url), {
+add_task(async function test_img() {
+  await BrowserTestUtils.withNewTab({ gBrowser }, async function (browser) {
+    browser.loadURI(Services.io.newURI(TEST_IMG_PATH), {
       triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
-      forceMediaDocument,
+      forceMediaDocument: "image",
     });
 
-    await BrowserTestUtils.browserLoaded(browser, false, url);
+    await BrowserTestUtils.browserLoaded(browser, false, TEST_IMG_PATH);
 
-    await SpecialPowers.spawn(browser, [], contentFn);
-  });
-}
-
-add_task(async function test_img() {
-  await doLoad(TEST_PATH + "file_image_header.sjs?imagePNG", "image", () => {
-    // The image was successfully displayed inline, which means
-    // we sent the right Accept header and ignored the Content-Disposition.
-    let img = content.document.querySelector("img");
-    is(img.width, 1, "PNG width");
-    is(img.height, 1, "PNG height");
-  });
-});
-
-add_task(async function test_img() {
-  await doLoad(TEST_PATH + "file_image_header.sjs?imageSVG", "image", () => {
-    let img = content.document.querySelector("img");
-    is(img.width, 100, "SVG width");
-    is(img.height, 100, "SVG height");
+    await SpecialPowers.spawn(browser, [], () => {
+      // The image was successfully displayed inline, which means
+      // we sent the right Accept header and ignored the Content-Disposition.
+      let img = content.document.querySelector("img");
+      is(img.width, 1);
+      is(img.height, 1);
+    });
   });
 });
