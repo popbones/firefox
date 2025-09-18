8b3b0f9370ff9fc91bd614da6fd240ea741b4290
Ben Hearsum <ben@mozilla.com>
2025-09-18 12:38:50 +0000

    Bug 1982882: fix gecko_taskgraph tests to work with multiprocess taskgraph r=taskgraph-reviewers,jcristau
    `GraphConfig` is sent into subprocesses, and needs to be serializable. We need our fake version to be implemented slightly different for this reason.
    
    Differential Revision: https://phabricator.services.mozilla.com/D264589

--- taskcluster/gecko_taskgraph/test/conftest.py
+++ taskcluster/gecko_taskgraph/test/conftest.py
@@ -119,11 +119,15 @@ class WithFakeKind(TaskGraphGenerator):
             yield FakeKind.create(kind_name, cfg, graph_config)
 
 
+class FakeGraphConfig(GraphConfig):
+    def register(self):
+        pass
+
+
 def fake_load_graph_config(root_dir):
-    graph_config = GraphConfig(
+    graph_config = FakeGraphConfig(
         {"trust-domain": "test-domain", "taskgraph": {}}, root_dir
     )
-    graph_config.__dict__["register"] = lambda: None
     return graph_config
 
 
