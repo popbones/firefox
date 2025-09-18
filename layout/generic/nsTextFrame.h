8a904f40615f3ad5ec47604dc7e68595978a52c0
Jonathan Kew <jkew@mozilla.com>
2025-09-18 11:27:29 +0000

    Bug 1989030 - patch 4 - Don't allow text-autospace to be applied in masked password fields. r=layout-jp-market-reviewers,layout-reviewers,emilio,TYLin
    Applying text-autospace (based on the underlying text) here could reveal hints about
    the structure of the password.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265113

--- layout/generic/nsTextFrame.h
+++ layout/generic/nsTextFrame.h
@@ -57,7 +57,7 @@ class MOZ_STACK_CLASS TextAutospace final {
 
   // Returns true if inter-script spacing may be added at boundaries.
   static bool Enabled(const StyleTextAutospace& aStyleTextAutospace,
-                      const nsIFrame* aFrame);
+                      const nsTextFrame* aFrame);
 
   TextAutospace(const StyleTextAutospace& aStyleTextAutospace,
                 nscoord aSpacing);
