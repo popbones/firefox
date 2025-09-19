4a2a504787404156aaecc7d41166e1f4f95e8982
iulian moraru <imoraru@mozilla.com>
2025-09-19 18:33:26 +0300

    Revert "Bug 1947324 part 3 - use WinRT/Windows App SDK instead of WS_SYSMENU to collapse titlebar r=emilio" for causing multiple failures.
    This reverts commit 032488ff117dfaba03f84e3f0108af11e63adf9d.
    
    Revert "Bug 1947324 part 2 - make Windows Firefox builds depend on the Windows App SDK r=glandium"
    
    This reverts commit 1ebdc110f24d33ed24d1e496b0306aa6248e3d93.
    
    Revert "Bug 1947324 part 1 - "build" Windows App SDK on toolchain tasks r=glandium"
    
    This reverts commit 37b6a1f956e68d91be2f6804a7b0352ea46b7da3.

--- widget/windows/WindowsUIUtils.h
+++ widget/windows/WindowsUIUtils.h
@@ -12,10 +12,6 @@
 #include "mozilla/Maybe.h"
 #include "mozilla/MozPromise.h"
 
-// Avoid including windef.h to get this, which improves
-// build times.
-typedef struct HWND__* HWND;
-
 using SharePromise =
     mozilla::MozPromise<bool, nsresult, /* IsExclusive */ true>;
 
@@ -59,7 +55,6 @@ class WindowsUIUtils final : public nsIWindowsUIUtils {
   static bool ComputeOverlayScrollbars();
   static double ComputeTextScaleFactor();
   static bool ComputeTransparencyEffects();
-  static void SetIsTitlebarCollapsed(HWND aWnd, bool aIsCollapsed);
 
  protected:
   ~WindowsUIUtils();
