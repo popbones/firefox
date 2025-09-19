3439e53eecf81f4dd5ff1f23356baba740e413bf
William Durand <will+git@drnd.me>
2025-09-19 08:41:40 +0000

    Bug 1989205 - Update recommended.xpi file to fix a test (permafailure). r=robwu
    The new XPI file has been generated from this PR [1], and signed by the
    AMO -dev instance (which uses the nonprod cert). FTR since this add-on
    needs a recommendation state, we must add the add-on to the "By Firefox"
    promoted group via the AMO admin before getting the version signed.
    
    Before:
    
    ```
    $ xpidump old.xpi
    MANIFEST:
      ID        : N/A
      Version   : 2.0
      Enterprise: No
    
    RECOMMENDATION:
      [Line]
    
    SIGNATURES:
      PKCS7:
       └── PRESENT / PRODUCTION / SHA-1 / REGULAR ADD-ON
       └── Certificates:
            └── Common Name         (CN): signingca1.addons.mozilla.org
                Organizational Unit (OU): Mozilla AMO Production Signing Service
                End Date                : 2025-04-04T00:00:00Z
            └── Common Name         (CN): {811d77f1-f306-4187-9251-b4ff99bad60b}
                Organizational Unit (OU): Production
                End Date                : 2030-09-15T16:50:45Z
      COSE:
       └── PRESENT / PRODUCTION / ES256 / REGULAR ADD-ON
       └── Certificates:
            └── Common Name         (CN): signingca1.addons.mozilla.org
                Organizational Unit (OU): Mozilla AMO Production Signing Service
                End Date                : 2025-04-04T00:00:00Z
            └── Common Name         (CN): {811d77f1-f306-4187-9251-b4ff99bad60b}
                Organizational Unit (OU): Production
                End Date                : 2030-09-15T16:50:45Z
    ```
    
    After:
    
    ```
    $ xpidump new.xpi
    MANIFEST:
      ID        : recommended-line@test.mozilla.org
      Version   : 3.0
      Enterprise: No
    
    RECOMMENDATION:
      [Line]
    
    SIGNATURES:
      PKCS7:
       └── PRESENT / STAGING / SHA-256 / REGULAR ADD-ON
       └── Certificates:
            └── Common Name         (CN): cas-intermediate-amo-ca-staging
                Organizational Unit (OU): Mozilla Staging Signing Service
                End Date                : 2050-12-31T00:00:00Z
            └── Common Name         (CN): recommended-line@test.mozilla.org
                Organizational Unit (OU): Production
                End Date                : 2035-09-17T07:29:49Z
      COSE:
       └── PRESENT / STAGING / ES256 / REGULAR ADD-ON
       └── Certificates:
            └── Common Name         (CN): cas-intermediate-amo-ca-staging
                Organizational Unit (OU): Mozilla Staging Signing Service
                End Date                : 2050-12-31T00:00:00Z
            └── Common Name         (CN): recommended-line@test.mozilla.org
                Organizational Unit (OU): Production
                End Date                : 2035-09-17T07:29:49Z
    ```
    
    [1]: https://github.com/mozilla/mozilla-central-test-xpis/pull/15
    
    Differential Revision: https://phabricator.services.mozilla.com/D265410

--- toolkit/mozapps/extensions/test/xpinstall/browser_doorhanger_installs.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_doorhanger_installs.js
@@ -651,7 +651,11 @@ describe("Add-on installation doorhangers", function () {
 
   it("should not show the 3rd party install panel when installing recommended add-ons", async function test_recommendedPostDownload() {
     await SpecialPowers.pushPrefEnv({
-      set: [["extensions.postDownloadThirdPartyPrompt", true]],
+      set: [
+        ["extensions.postDownloadThirdPartyPrompt", true],
+        // recommended.xpi is signed with AMO staging signature.
+        ["xpinstall.signatures.dev-root", true],
+      ],
     });
 
     let xpiURL = TESTROOT + "recommended.xpi";
@@ -664,7 +668,7 @@ describe("Add-on installation doorhangers", function () {
 
     let notificationPromise = acceptAppMenuNotificationWhenShown(
       "addon-installed",
-      "{811d77f1-f306-4187-9251-b4ff99bad60b}"
+      "recommended-line@test.mozilla.org"
     );
 
     installDialog.button.click();
@@ -674,7 +678,7 @@ describe("Add-on installation doorhangers", function () {
     is(installs.length, 0, "Should be no pending installs");
 
     let addon = await AddonManager.getAddonByID(
-      "{811d77f1-f306-4187-9251-b4ff99bad60b}"
+      "recommended-line@test.mozilla.org"
     );
     await addon.uninstall();
 
