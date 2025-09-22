eafe10d94e8027139538858aa0ca6cb393acbe79
Ting-Yu Lin <tlin@mozilla.com>
2025-09-22 11:16:44 +0000

    Bug 1989851 Part 1 - Avoid unnecessary findPrecedingClass() calls. r=jfkthame,layout-jp-market-reviewers,layout-reviewers,emilio
    `valueOr()` unconditionally evaluates its parameter, so we should use
    `valueOrFrom()` to call `findPrecedingClass()` only when needed.
    See the documentation:
    https://searchfox.org/firefox-main/rev/36ebbdf6fbbfd0b40a607212ec843992b5d43e92/mfbt/Maybe.h#504-509
    
    With `layout.css.text-autospace.enabled=true`, on my local Linux machine, the
    running time of
    `testing/talos/talos/tests/perf-reftest-singletons/svg-text-textLength-1.html`
    drops from 29,425ms to 22,279ms (~24% faster).
    
    Differential Revision: https://phabricator.services.mozilla.com/D265562

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -4133,7 +4133,7 @@ void nsTextFrame::PropertyProvider::GetSpacingInternal(Range aRange,
             // current class is `Other`, which never participates in spacing.
             if (!atStart && currClass != CharClass::Other &&
                 mTextAutospace->ShouldApplySpacing(
-                    prevClass.valueOr(findPrecedingClass()), currClass)) {
+                    prevClass.valueOrFrom(findPrecedingClass), currClass)) {
               aSpacing[runOffsetInSubstring + i].mBefore +=
                   mTextAutospace->InterScriptSpacing();
             }
